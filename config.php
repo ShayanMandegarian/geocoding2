ec2c1557f72fadb60bb46ca83f173a8167440bbf
Shayan Mandegarian <smandegarian@yahoo.com>
2018-09-14 15:45:01 -0700

    removed keys

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$key = 'AIzaSyBgFEGhTyZ7Qhd955EtDx-xrBZRyIPK7Rs';
-$hereID = 'WhTigBXTCVjwYr8QEdYw';
-$hereCode = 'VpiXBaY0UeyqK9dHJ4fO0A';
+$key = '';
+$hereID = '';
+$hereCode = '';
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
